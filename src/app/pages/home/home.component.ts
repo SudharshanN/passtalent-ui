2c90976d964e422c13c53cf58f93c03ccf0df13b
Sharief123 <janisherry9666@gmail.com>
2021-12-30 22:01:20 +0530

    Done Rating in Home Page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,4 +12,8 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onClick() {
+    alert("You have Pressed a Button to See More Supported By Schools && Colleges");
+  }
+
 }
